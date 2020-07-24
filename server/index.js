3554b126b8eb9d91f0871a1ac7e215157df3cbc5
aarond-897 <aaronda2011@gmail.com>
2020-07-24 13:18:32 -0700

    boiler plate index with auth and finnhub endpoints

---
+++ server/index.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+//connect to controllers
+const authCtrl = require('./controllers/authCtrl'),
+      userCtrl = require('./controllers/userCtrl'),
+      finnhubCtrl = require('./controllers/finnhubCtrl');
+
+//server boilerplate
+const express = require('express'),
+      massive = require('massive'),
+      session = require('express-session'),
+      {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET}=process.env,
+      port=SERVER_PORT,
+      app=express();
+app.use(express.json());
+
+//session and cookie creation
+app.use(session({
+    resave:false,
+    saveUninitialized:true,
+    secret: SESSION_SECRET,
+    cookie:{maxAge:1000*60*60*24*365}
+}));
+
+//connect to db
+massive({
+    connectionString:CONNECTION_STRING,
+    ssl:{rejectUnauthorized: false}
+}).then(db=>{
+    app.set('db',db);
+    console.log('db connected')
+}).catch(err=>console.log(err));
+
+//auth endpoints
+app.post('/auth/register',authCtrl.register);
+app.post('/auth/login',authCtrl.login);
+app.get('/auth/logout', authCtrl.logout);
+
+//user endpoints
+app.post('/api/ticker/:ticker', finnhubCtrl.getFinInfo)
+//finnHub endpoint
+
+app.listen(port, ()=>console.log(`Connected on port ${port}`))
